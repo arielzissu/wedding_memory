c69387359da4b3a2a8439eed7b0d39c3c8fe737f
arielzissu8 <ariel.zissu@plusgrade.com>
2025-01-12 19:53:30 +0200

    fixed env

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,21 +5,25 @@ import dotenv from 'dotenv';
 import router from './routes';
 import { PORT, PUBLIC_URL } from './constants';
 
+dotenv.config();
+
 const app: Application = express();
 
 app.use(
   PUBLIC_URL,
-  express.static(path.resolve(__dirname, '../../build'), { maxAge: Infinity })
+  express.static(path.resolve(__dirname, '../../build'), { maxAge: '1y' })
 );
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-dotenv.config();
-
 app.use('/api', router);
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () =>
+    console.log(`Server running locally on port: ${PORT}`)
+  );
+}
 
-module.exports = app;
+export default app;
