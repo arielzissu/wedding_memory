b7da1bbd4ca922d7bd2afab9dfc4b3669c978062
arielzissu8 <ariel.zissu@plusgrade.com>
2025-01-12 22:43:35 +0200

    new env API_URL

--- src/client/utils/api.ts
+++ src/client/utils/api.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import { API_URL } from '../constants';
 
 export const request = async ({ url, uri, method, ...rest }: any) => {
   try {
     const response = await axios({
       method: method || 'GET',
-      url: url || `${process.env.REACT_APP_PROXY_URL}/api${uri}`,
+      url: url || `${API_URL}/api${uri}`,
       ...rest
     });
 
