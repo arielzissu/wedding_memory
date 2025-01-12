1cff6cb1289dcf8219a466ca8b23044c82f99078
arielzissu8 <ariel.zissu@plusgrade.com>
2025-01-12 22:09:46 +0200

    added hello route

--- src/server/routes/cloudinary.ts
+++ src/server/routes/cloudinary.ts
@@ -7,8 +7,9 @@ const router = express.Router();
 
 const upload = multer({ storage: cloudinaryStorage });
 
-router.post('/upload', upload.array('images'), uploadImages);
+router.get('/hello', (_req, res) => res.json({ message: 'hello' }));
 router.get('/images', getImages);
+router.post('/upload', upload.array('images'), uploadImages);
 router.delete('/image', deleteImage); // TODO: Validate route by google token
 
 export default router;
